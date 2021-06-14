9b854e4200de9f2e66c933977e41d1e23771c5cd
genesis-kim <genesis.kim@navercorp.com>
2021-06-15 02:20:20 +0900

    First submission of React-Native

--- App.tsx
+++ App.tsx
@@ -1,12 +1,38 @@
+
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from "expo-font";
+import { Asset } from 'expo-asset';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false)
+  const preload = async() => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font: any) => Font.loadAsync(font));
+    const imagesToLoad = [
+      require("./assets/logo.png"),
+      "https://cdn.iconscout.com/icon/premium/png-512-thumb/ice-coffee-1-642893.png",
+    ];
+    const imagePromises = imagesToLoad.map((image: any) => Asset.loadAsync(image));
+    await Promise.all([fontPromises, imagePromises]);
+  }
+  if (loading) {
+    return (
+      <AppLoading 
+        startAsync={preload}
+        onError={console.warn} 
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello!</Text>
+      <StatusBar style="auto" /> 
     </View>
   );
 }
