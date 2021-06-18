ebd61d3a69855227c9f795d27bad5c57882f4db6
genesis.kim <genesis.kim@navercorp.com>
2021-06-19 04:19:50 +0900

    Second Submmit React-native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 
 import AppLoading from 'expo-app-loading';
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import * as Font from "expo-font";
 import { Asset } from 'expo-asset';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import LoggedOutNav from './navigators/LoggedOutNav';
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -30,18 +30,8 @@ export default function App() {
     );
   }
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" /> 
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
